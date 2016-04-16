bd80d7f03bd2f20fb5c60173965cadee502cb153
sircraftsalot111 <sircraftsalot111@gmail.com>
2016-04-16 19:44:58 -0400

    ui-login,reg,forget

--- smartallowance/www/views/home/home.js
+++ smartallowance/www/views/home/home.js
@@ -1,16 +1,18 @@
 'Use Strict';
 angular.module('App').controller('homeController', function ($scope, $state,$cordovaOauth, $localStorage, $location,$http,$ionicPopup, $firebaseObject, Auth, FURL, Utils) {
   var ref = new Firebase(FURL);
-  var profileRef = new Firebase(FURL + "/profile");
   console.log(ref.getAuth())
   var userId = ref.getAuth() != null ? ref.getAuth().uid : null;
   if (userId == null) { console.log("User is not authenticated"); }
+  var profileRef = new Firebase(FURL + "/profile" + "/" + userId);
 
   profileRef.on("value", function(snap) {
 	  	console.log(snap.val());
         var users = snap.val();
         var key = Object.keys(users);
-		$scope.currentUserName = users[key[0]].name;
+
+
+		      $scope.currentUserName = users.name;
   }, function (errorObject) {
 	  	console.log("The read failed: " + errorObject.code);
   });
